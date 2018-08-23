6bf394556fee52c5048e879b79a213e33e5c53df
Kevin Rosendahl <kevindrosendahl@gmail.com>
2018-08-22 23:17:53 -0700

    automatically build all protos in proto/protod

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 extern crate tower_grpc_build;
+use std::fs;
 
 fn main() {
     // Build protod
+    let protod_paths = fs::read_dir("./proto/protod");
+    let protod_files : Vec<String> = protod_paths.unwrap()
+        .map(|r| r.unwrap())
+        .map(|d| format!("proto/protod/{}", d.path().file_name().unwrap().to_str().unwrap()))
+        .collect();
+
     tower_grpc_build::Config::new()
         .enable_server(true)
-        .build(&["proto/protod/database_create.proto", "proto/protod/protod.proto"], &["proto"])
+        .build(&protod_files, &[String::from("proto")])
         .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
