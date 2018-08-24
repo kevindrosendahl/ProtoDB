ee61b4185f73510891ffba75c34e6382d16be550
Kevin Rosendahl <kevindrosendahl@gmail.com>
2018-08-23 18:17:22 -0700

    remove default kv_storage_engine impl

--- src/storage/in_memory/mod.rs
+++ src/storage/in_memory/mod.rs
@@ -2,7 +2,10 @@ use std::collections::BTreeMap;
 use std::ops::Bound;
 use std::sync::{Arc, RwLock};
 
-use storage::kv::KVStorageEngine;
+use storage::storage_engine::StorageEngine;
+
+const DATABASES_PREFIX: &'static str = "databases";
+const DELIMITER: &'static str = "/";
 
 pub struct InMemoryStorageEngine {
     cache: Arc<RwLock<BTreeMap<Vec<u8>, Vec<u8>>>>
@@ -12,9 +15,8 @@ impl InMemoryStorageEngine {
     pub fn new() -> InMemoryStorageEngine {
         InMemoryStorageEngine { cache: Arc::new(RwLock::new(BTreeMap::new())) }
     }
-}
 
-impl KVStorageEngine for InMemoryStorageEngine {
+    #[allow(dead_code)]
     fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
         let cache = self.cache.clone();
         let cache = cache.read().unwrap();
@@ -34,5 +36,21 @@ impl KVStorageEngine for InMemoryStorageEngine {
     fn put(&self, key: &[u8], value: &[u8]) {
         let cache = self.cache.clone();
         cache.write().unwrap().insert(key.to_vec(), value.to_vec());
+    }
+}
+
+impl  StorageEngine for InMemoryStorageEngine {
+    fn list_databases(&self) -> Vec<String> {
+        let lower = format!("{}{}", DATABASES_PREFIX, DELIMITER);
+        let delimiter_plus_1 = DELIMITER.as_bytes()[0] + 1;
+        let upper = format!("{}{}", DATABASES_PREFIX, delimiter_plus_1);
+
+        let databases = self.get_range(lower.as_bytes(), upper.as_bytes()).unwrap();
+        databases.iter().map(|b| String::from_utf8(b.clone()).unwrap()).collect()
+    }
+
+    fn create_database(&self, name: &str) {
+        let key = format!("{}{}{}", DATABASES_PREFIX, DELIMITER, name);
+        self.put(key.as_bytes(), name.as_bytes());
     }
 }
