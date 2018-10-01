1a3bcce3586f00cab12233b1ebb36e51dadd212a
Kevin Rosendahl <kevindrosendahl@gmail.com>
2018-10-01 01:04:37 -0700

    clean up main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use libprotodb;
+use libprotodb::{
+    grpc::{
+        handler::Handler,
+        server::Server,
+    },
+    storage::in_memory::InMemoryStorageEngine,
+};
 
 pub fn main() {
-    let storage_engine = libprotodb::storage::in_memory::InMemoryStorageEngine::new();
-    let handler = libprotodb::grpc::handler::Handler::new(Box::new(storage_engine));
-    let server = libprotodb::grpc::server::Server::new("127.0.0.1:10000".parse().unwrap(), handler);
+    let storage_engine = InMemoryStorageEngine::new();
+    let handler = Handler::new(Box::new(storage_engine));
+    let server = Server::new("127.0.0.1:10000".parse().unwrap(), handler);
     server.run();
 }
