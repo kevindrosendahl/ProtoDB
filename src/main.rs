40e8ad8f61f367e4c863d614b34150b7861c36e8
Kevin Rosendahl <kevindrosendahl@gmail.com>
2018-12-17 17:02:56 -0800

    change default data directory to /var/lib/protodb/data, add flag to configure it

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,24 @@ use protodb::{
     storage::engine::rocksdb::RocksDBStorageEngine,
     transport::grpc::{handler::Handler, server::Server},
 };
+use clap::{Arg, App};
 
 pub fn main() {
     pretty_env_logger::init();
+    let matches = App::new("protodb")
+        .version("0.1.0")
+        .author("Kevin Rosendahl <kevindrosendahl@gmail.com>")
+        .arg(Arg::with_name("data-dir")
+            .long("data-dir")
+            .value_name("DATA-DIR")
+            .help("root directory to store data in")
+            .takes_value(true)
+            .default_value("/var/lib/protodb/data"))
+        .get_matches();
 
-    let storage_engine = RocksDBStorageEngine::try_new("/tmp/protodb/data");
+    let data_dir = matches.value_of("data-dir").unwrap();
+
+    let storage_engine = RocksDBStorageEngine::try_new(data_dir);
     let handler = Handler::new(Arc::new(storage_engine.unwrap_or_else(|err| {
         panic!(format!("error initializing RocksDB database: {}", err));
     })));
--- src/storage/engine/rocksdb/kv_store.rs
+++ src/storage/engine/rocksdb/kv_store.rs
@@ -12,8 +12,10 @@ pub struct RocksDBKVStore {
 
 impl RocksDBKVStore {
     pub fn try_new(path: &str) -> Result<RocksDBKVStore, rocksdb::Error> {
+        let mut opts = rocksdb::Options::default();
+        opts.create_if_missing(true);
         Ok(RocksDBKVStore {
-            inner: Arc::new(rocksdb::DB::open_default(path)?),
+            inner: Arc::new(rocksdb::DB::open(&opts, path)?),
         })
     }
 }
@@ -42,9 +44,18 @@ impl KVStore for RocksDBKVStore {
             rocksdb_batch.put(key, value).unwrap();
         }
 
+        // RocksDB uses a WAL to ensure durability.
+        // We use disable_wal(false) to ensure that the WAL won't be disabled for some reason
+        // (it is enabled by default), and set_sync(true) to ensure that RocksDB calls `fsync`
+        // prior to returning from `write_opt`.
+        // For more information read: https://github.com/facebook/rocksdb/wiki/Basic-Operations#synchronous-writes
+        let mut opts = rocksdb::WriteOptions::default();
+        opts.disable_wal(false);
+        opts.set_sync(true);
+
         self.inner
             .clone()
-            .write(rocksdb_batch)
+            .write_opt(rocksdb_batch, &opts)
             .map_err(|err| err.into())
     }
 
